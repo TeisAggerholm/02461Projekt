85daec8a76cd075a5a7dd29bec10b6c443e151ca
hectorjak <hectorheltj@gmail.com>
2024-01-02 16:24:36 +0100

    h

--- Data.py
+++ Data.py
@@ -6,10 +6,10 @@ sumo_config_path = 'sumo_files/osm.sumocfg'
 
 traci.start(["sumo", "-c", sumo_config_path])
 
-# for step in range(10):
-#     traci.simulationStep()
+for step in range(10):
+     traci.simulationStep()
 
-#     vehicle_positions = traci.vehicle.getPosition("vehicle_id")
-#     vehicle_speed = traci.vehicle.getSpeed("vehicle_id")
+     vehicle_positions = traci.vehicle.getPosition("vehicle_id")
+     vehicle_speed = traci.vehicle.getSpeed("vehicle_id")
 
-# traci.close()
+traci.close()
