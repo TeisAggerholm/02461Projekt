44c62c63fafc51795a6db90839dfc6b74b6e5d8d
TeisAggerholm <TeisAggerholm@hotmail.com>
2024-01-02 16:19:07 +0100

    sumo_config_path

--- Data.py
+++ Data.py
@@ -1,8 +1,10 @@
 import traci
-user = ["hectorheltjakobsen"]
-path = rf"/Users/{user[0]}/sumo/tools/2024-01-02-14-05-12/osm.sumocfg"
+import os
 
-traci.start(["sumo", "-c", path])
+
+sumo_config_path = 'sumo_files/2024-01-02-14-05-12/osm.sumocfg'
+
+traci.start(["sumo", "-c", sumo_config_path])
 
 for step in range(10):
     traci.simulationStep()
