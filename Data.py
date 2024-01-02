af9c1bb669d97563af8b50be2e9b5ad137076fad
hectorjak <hectorheltj@gmail.com>
2024-01-02 15:46:16 +0100

    hej

--- Data.py
+++ Data.py
@@ -1,7 +1,8 @@
 import traci
+user = ["hectorheltjakobsen"]
+path = rf"/Users/{user[0]}/sumo/tools/2024-01-02-14-05-12/osm.sumocfg"
 
-traci.start(["sumo", "-c", "/Users/hectorheltjakobsen/sumo/tools/2024-01-02-14-05-12/osm.sumocfg"])
-
+traci.start(["sumo", "-c", path])
 
 for step in range(10):
     traci.simulationStep()
