6bfabeed3eed20fff883e6c2571b5929053dae2b
hectorjak <hectorheltj@gmail.com>
2024-01-03 14:57:50 +0100

    simulation

--- support/simulation.py
+++ support/simulation.py
@@ -1,12 +1,64 @@
+import traci
+import os
 
-class Simulation():
-    def __init__():
-        pass
+class simulation:
+     waiting_times = {}
+     co2_emission = 0
+     sum_queue = 0
+     def __init__(self, sumo_path, max_step):
+          self.sumo_path = sumo_path
+          self.max_step = max_step
+     
+     def run(self):
+          traci.start(["sumo", "-c", self.sumo_path])
+          for step in range(self.max_step):
+               traci.simulationStep()
+               vehicles = traci.vehicle.getIDList()
+               for vehicle in vehicles:
+                    simulation(self.sumo_path,self.max_step).get_waiting_time(vehicle)
+                    self.co2_emission += simulation(self.sumo_path,self.max_step).get_co2_emission(vehicle)
 
-    def run():
-        pass
+                    self.sum_queue += sum(simulation(self.sumo_path,self.max_step).get_queue_length())
+          
+          total_waiting_time = sum(self.waiting_times.values())
 
-    def _get_state():
-        pass
-    def _get_score():
-        pass
+          avg_wait_time = total_waiting_time/simulation(self.sumo_path,self.max_step).count_halting_vehicles()[0]
+          halting_vehicle_count = simulation(self.sumo_path,self.max_step).count_halting_vehicles()[1]
+          co2_emission_total = round(self.co2_emission/1000)
+          avg_queue_length = self.sum_queue/self.max_step
+
+          traci.close()
+
+          return avg_wait_time, halting_vehicle_count, co2_emission_total, avg_queue_length
+
+     def get_queue_length(self): 
+          Halt_N = traci.edge.getLastStepHaltingNumber("-125514711")
+          Halt_S = traci.edge.getLastStepHaltingNumber("125514709")
+          Halt_E = traci.edge.getLastStepHaltingNumber("548975769")
+          Halt_W = traci.edge.getLastStepHaltingNumber("-125514713")
+          
+          return Halt_N, Halt_S, Halt_E, Halt_W
+
+     def get_waiting_time(self, vehicle):
+          self.vehicle = vehicle
+          wait_time = traci.vehicle.getAccumulatedWaitingTime(self.vehicle)
+          self.waiting_times[self.vehicle] = wait_time
+     
+     def get_co2_emission(self,vehicle):
+          self.vehicle = vehicle
+          co2_emission_car = traci.vehicle.getCO2Emission(self.vehicle)
+          return co2_emission_car
+          
+
+     def count_halting_vehicles(self):
+          total_vehicle_count = 0
+          halting_count = 0
+          for value in self.waiting_times.values():
+               total_vehicle_count += 1
+               if value != 0:
+                    halting_count += 1
+          return total_vehicle_count, halting_count
+     
+if __name__ == '__main__':
+     print("These are the total stats of the run")
+     print(simulation('sumo_files/osm.sumocfg', 200).run())
