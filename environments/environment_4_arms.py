1aed5019115b9b41e839308b37c65a17186a3f47
TeisAggerholm <TeisAggerholm@hotmail.com>
2024-01-03 10:43:44 +0100

    ss

--- environments/environment_4_arms.py
+++ environments/environment_4_arms.py
@@ -1,3 +1,8 @@
 class environment_4_arms:
+    # Skal give hvilket antal mulige actions der er.
+    # Skal håndtere hvadd der sker ved hver action. Og heri ændre lyskrydsene.
+    # Skal gul phase.
+
+
     def __init__(self):
         pass
