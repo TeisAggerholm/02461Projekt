66cf969c12fefd2740ce9d06f4e48cda1dd5c177
TeisAggerholm <TeisAggerholm@hotmail.com>
2024-01-04 12:55:48 +0100

    x

--- app.py
+++ app.py
@@ -13,12 +13,14 @@ red_phase_steps = settings["environment"]["red_phase_steps"]
 
 # Environment
 environment_name = settings['environment']['name']
-environment = getattr(importlib.import_module(f'environments.{environment_name.lower()}'), environment_name)(sumo_mode, min_green_phase_steps, yellow_phase_steps, red_phase_steps) 
+environment_class = getattr(importlib.import_module(f'environments.{environment_name.lower()}'), environment_name)
+environment = environment_class(sumo_mode, min_green_phase_steps, yellow_phase_steps, red_phase_steps) 
 
 # Model
 model_args = settings["model"]["*args"]
 model_name = utils.read_settings('settings.json')['model']['name']
-model = getattr(importlib.import_module(f'models.{model_name.lower()}'), model_name)(environment.num_actions, *model_name)
+model_class = getattr(importlib.import_module(f'models.{model_name.lower()}'), model_name)
+model = model_class(environment.num_actions, *model_name)
 
 # Simulation
 Simulation(max_step, environment, model)
